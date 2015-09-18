2a64ad72fac651ed8aeeaa6d191dc75c41016d26
Abby Greenbloom <abby.greenbloom@gmail.com>
2015-09-18 16:47:58 -0400

    move code to correct file

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -1,2 +1,21 @@
 class PeopleController < ApplicationController
+    def index
+    render json: @people
+  end
+
+  def show
+    render json: @person.id
+  end
+
+  private
+
+  def search_people
+    @people = if params[:search].present?
+      Person.seach(params[:search])
+    else
+      Person.all
+    end
+  end
+
+
 end
--- app/models/person.rb
+++ app/models/person.rb
@@ -1,21 +1,3 @@
 class Person < ActiveRecord::Base
 
-  def index
-    render json: @people
-  end
-
-  def show
-    render json: @person.id
-  end
-
-  private
-
-  def search_people
-    @people = if params[:search].present?
-      Person.seach(params[:search])
-    else
-      Person.all
-    end
-  end
-
 end
